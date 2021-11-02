3d7f62d257f1acefe7742277b3c0c068a55a00d8
kyosuke ito <kyosuke.ito@gmail.com>
2021-11-02 13:55:29 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Display from "./Display";
+
+function App() {
+  return (
+    <div>
+      <h1>The Power of props</h1>
+      <Display text="Props is often used in React" />
+      <Display text="It is likely nice" />
+      <Display text="How good?" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Display.js
@@ -0,0 +1,5 @@
+function Display(props) {
+  return <h2>{props.text}</h2>;
+}
+
+export default Display;
